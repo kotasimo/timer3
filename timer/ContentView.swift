2c9a1f6f07ce2380b7100598fab182881180ccce
simo <1804Naporeon>
2026-02-21 12:34:34 +0900

    Initial Commit

---
+++ timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  timer
+//
+//  Created by Apex_Ventura on 2026/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by Apex_Ventura on 2026/02/21.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
